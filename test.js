11444f8baf391fbb4c640411629c7797ae21f4b4
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2020-05-09 23:25:39 +0900

    Fix test

--- test.js
+++ test.js
@@ -6,16 +6,27 @@ const urlA = "https://kamataryo.github.io/wwwdiff/sample-a.html";
 const urlB = "https://kamataryo.github.io/wwwdiff/sample-b.html";
 
 const main = async () => {
-  {
-    const diff = await w3Diff(urlA, urlB, { color: "#ff00ff" });
-    const snapshot = await fs.promises.readFile("./sample-diff.png");
-    assert.deepEqual(diff, snapshot);
+  try {
+    {
+      const diff = await w3Diff(urlA, urlB, { color: "#ff00ff" });
+      const snapshot = await fs.promises.readFile("./__test__/sample-diff.png");
+      assert.deepEqual(diff, snapshot);
+    }
+    {
+      const sampleA = await w3Diff(urlA, void 0, { color: "#ff00ff" });
+      const snapshot = await fs.promises.readFile("./__test__/sample-a.png");
+      assert.deepEqual(sampleA, snapshot);
+    }
+    {
+      const sampleA = await w3Diff(urlA, void 0, { color: "#ff00ff" });
+      const snapshot = await fs.promises.readFile("./__test__/sample-b.png");
+      assert.deepEqual(sampleA, snapshot);
+    }
+  } catch (error) {
+    process.stderr.write(error);
+    process.exit(1);
   }
-  {
-    const sampleA = await w3Diff(urlA, void 0, { color: "#ff00ff" });
-    const snapshot = await fs.promises.readFile("./sample-a.png");
-    assert.deepEqual(sampleA, snapshot);
-  }
+  process.exit(0);
 };
 
 main();
